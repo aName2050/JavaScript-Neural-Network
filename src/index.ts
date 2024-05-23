8921c7811eba5e85988a0f54c3e539bd8b048e1a
aName <shafaeebardia@gmail.com>
2024-05-23 16:03:14 +0000

    prepped for MNIST dataset

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,4 @@ import Dataset from '../Config/Data/dataset.json';
 import { output } from './Util/output';
 import { ActivationFunction } from './Util/activation';
 
-const nn = new NeuralNetwork(1, [5, 5, 5], 1, ActivationFunction.LeakyReLU);
-
-const testData = [3]; // expect: 3.4
-
-// pre-training
-console.log('PRE-TRAINING...');
-output(nn.forwardPropagation(testData));
-// console.log(nn.forwardPropagation(testData));
-
-// training
-const { data } = Dataset;
-
-for (let i = 0; i < learnCycles; i++) {
-	// console.log(`Learn cycle (${i + 1}/${learnCycles})`);
-	for (let d = 0; d < data.length; d++) {
-		// console.log(`Data (${d + 1}/${data.length})`);
-		nn.train(data[d].input, data[d].output, learnRate);
-	}
-}
-
-// post-training
-console.log('POST-TRAINING...');
-console.log('3+0.4');
-output(nn.forwardPropagation(testData));
-console.log('5.8+0.4'); // expect 6.2
-output(nn.forwardPropagation([5.8]));
-console.log('50+0.4'); // expect 50.4
-output(nn.forwardPropagation([50]));
-// console.log(nn.forwardPropagation(testData));
+const nn = new NeuralNetwork(28 * 28, [16, 16], 10, ActivationFunction.Sigmoid);
