1e03e8e6750c054b85670cf9e7431974f5dd619b
aName <shafaeebardia@gmail.com>
2024-05-24 17:21:39 +0000

    begun implementation of showcase

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,18 @@ import { ActivationFunction } from './Util/activation';
 import { MNIST } from './Util/importer';
 
 const nn = new NeuralNetwork(28 * 28, [16, 16], 10, ActivationFunction.Sigmoid);
+
+const mnist = new MNIST();
+mnist.preprocess();
+
+const dataset = mnist.DATASET;
+
+const trainStart = Date.now();
+console.log('Starting network training...');
+// train
+for (let i = 0; i < learnCycles; i++) {
+	for (let j = 0; j < dataset.data.length; j++) {
+		nn.train(dataset.data[j].input, dataset.data[j].output, learnRate);
+	}
+}
+console.log(`Training finished in ${Date.now() - trainStart}ms`);
